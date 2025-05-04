9e9a20744fdb809442b63cf24554a0c921b6946d
xnought <drbertucci@hotmail.com>
2025-05-04 02:01:59 -0400

    count frequencies divide by total

--- main.go
+++ main.go
@@ -5,6 +5,22 @@ import (
 	"os"
 )
 
+func normalize_frequencies(freqs map[byte]uint64) map[byte]float64 {
+	normalized := map[byte]float64{} // char -> count/total_count
+
+	// total_count is sum of all frequencies
+	var total_count uint64 = 0
+	for _, count := range freqs {
+		total_count += count
+	}
+
+	// divide and save to normalized (also return this)
+	for char, count := range freqs {
+		normalized[char] = float64(count) / float64(total_count)
+	}
+	return normalized
+}
+
 func count_frequencies(data []byte) map[byte]uint64 {
 	char_count := map[byte]uint64{} // char -> count
 	for _, d := range data {
@@ -23,8 +39,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	out := count_frequencies(data)
-	for k, v in range out {
-		fmt.Printf("%c=%d\n", k, v);
+	out := normalize_frequencies(count_frequencies(data))
+	for k, v := range out {
+		fmt.Printf("'%c'=%f\n", k, v)
 	}
 }

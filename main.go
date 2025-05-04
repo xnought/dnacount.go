eb3d4a5cec954389a4a024b288505237d931caa2
xnought <drbertucci@hotmail.com>
2025-05-04 01:34:22 -0400

    print out file contents

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main()  {
-	fmt.Println("Hello World!");
+	data, err := os.ReadFile("main.go")
+	if err != nil {
+		panic(err)
+	}
+	for i, d := range data {
+		if d == '\n' {
+			fmt.Printf("%d: '\\n'\n", i)
+		} else if d == '\t' {
+			fmt.Printf("%d: '\\t'\n", i)
+		} else {
+			fmt.Printf("%d: '%c'\n", i, d)
+		}
+	}
 }

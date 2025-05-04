82017deee2c2c0aa8893cba6037199cb326203ad
xnought <drbertucci@hotmail.com>
2025-05-04 01:49:51 -0400

    print character frequencies

--- main.go
+++ main.go
@@ -5,18 +5,24 @@ import (
 	"os"
 )
 
-func main()  {
+func count_frequencies(data []byte) map[byte]uint64 {
+	char_count := map[byte]uint64{} // char -> count
+	for _, d := range data {
+		_, key_exists := char_count[d]
+		if key_exists {
+			char_count[d]++
+		} else {
+			char_count[d] = 1
+		}
+	}
+	return char_count
+}
+
+func main() {
 	data, err := os.ReadFile("main.go")
 	if err != nil {
 		panic(err)
 	}
-	for i, d := range data {
-		if d == '\n' {
-			fmt.Printf("%d: '\\n'\n", i)
-		} else if d == '\t' {
-			fmt.Printf("%d: '\\t'\n", i)
-		} else {
-			fmt.Printf("%d: '%c'\n", i, d)
-		}
-	}
+	out := count_frequencies(data)
+	fmt.Println(out)
 }
